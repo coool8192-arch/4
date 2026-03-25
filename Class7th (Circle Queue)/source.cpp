276eeb49d23963a4a78b17cd4d2d77f871e69ef4
coool8192-arch <coool8192@gmail.com>
2026-03-25 21:42:45 +0900

    260325 우선순위 큐

---
+++ Class7th (Circle Queue)/source.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+using namespace std;
+
+#define size 4
+
+template <typename T>
+class Queue
+{
+	T container[size];
+	int rear;
+	int front;
+
+public:
+	Queue()
+	{
+		rear = size - 1;
+		front = size - 1;
+		for (int i = 0; i < size; i++)
+		{
+			container[i] = NULL;
+		}
+	}
+
+	void push(T data)
+	{
+		if (front == (rear + 1) % size) // 원형 queue는 한 칸을 비워두기 때문에 이렇게 해야한다.
+		{
+			cout << "Circle queue overflow" << endl;
+		}
+		else
+		{
+			rear = (rear + 1) % size;
+
+			container[rear] = data;
+		}
+	}
+
+	void pop()
+	{
+		if (empty())
+		{
+			cout << "Circle queue is empty" << endl;
+		}
+		else
+		{
+			front = (front + 1) % size;
+
+			container[front] = NULL;
+		}
+	}
+
+	const bool& empty()
+	{
+		return front == rear;
+	}
+
+	const T& peek()
+	{
+		return container[(front + 1) % size];
+	}
+};
+
+int main()
+{
+	Queue<int> q;
+
+	q.push(10);
+	q.push(20);
+	q.push(30);
+	q.push(40);
+
+	while (!q.empty())
+	{
+		cout << q.peek() << endl;
+		q.pop();
+	}
+
+	q.push(20);
+	q.push(30);
+	q.push(40);
+	q.push(50);
+
+	while (!q.empty())
+	{
+		cout << q.peek() << endl;
+		q.pop();
+	}
+
+	return 0;
+}
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,52 +1,9 @@
 ﻿#include <iostream>
-#define size 5
-
 using namespace std;
-
-template <typename T>
-class Queue
-{
-	T container[size];
-	int rear;
-	int front;
-
-public:
-	Queue()
-	{
-		rear = 0;
-		front = 0;
-
-		for (int i = 0; i < size; i++)
-		{
-			container[i] = NULL;
-		}
-	}
-
-	void push(T data)
-	{
-		if (rear >= size)
-		{
-			cout << "linear queue overflow" << endl;
-		}
-		else
-		{
-			container[rear++] = data;
-		}
-	}
-
-};
 
 int main()
 {
-	Queue<int> queue;
 
-	queue.push(10);
-	queue.push(20);
-	queue.push(30);
-	queue.push(40);
-	queue.push(50);
-	queue.push(60);
-
-	return 0;
+return 0;
 }
 
